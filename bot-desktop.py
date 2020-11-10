1ed828ad84b5773db4c543dbaed46b0d32b017b2
Abelardo <37087157+avillalobosd@users.noreply.github.com>
2020-11-10 09:56:13 -0600

    Add files via upload

--- bot-desktop.py
+++ bot-desktop.py
@@ -6,15 +6,25 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import time
 import threading  
+from io import BytesIO
+import win32clipboard
+from PIL import Image
 switch = True  
-
 os.system('clear')
 
 
+#### CONGIRUACION PANTALLA DE WEB APP
 opts = Options()
 opts.add_experimental_option('debuggerAddress', 'localhost:9250')
 driver = webdriver.Chrome(options=opts)
 seguir = True
+
+#### CONGIRUACION PANTALLA DE WEB APP
+optsWhats = Options()
+optsWhats.add_experimental_option('debuggerAddress', 'localhost:9222')
+driverWhatsapp = webdriver.Chrome(options=optsWhats)
+seguir = True
+
 iteraciones=0
 
 #######################################################################
@@ -42,12 +52,7 @@ def aumentaMinimoCompraYa():
 #CLICK EN DISMINUIR EL PRECIO MINIMO
 def disminuyeMinimoCompraYa():
     driver.find_element_by_xpath("/html/body/main/section/section/div[2]/div/div[2]/div/div[1]/div[2]/div[5]/div[2]/button[1]").click()
-
-
-#######################################################################
-#######################################################################
-#######################################################################    
-#######################################################################    
+ 
 
 
 #######################################################################
@@ -62,11 +67,19 @@ def clickEncontrado():
         driver.find_element_by_xpath("/html/body/main/section/section/div[2]/div/div/section[1]/div/ul/li/div").click()
         print(driver.find_element_by_xpath("/html/body/main/section/section/div[2]/div/div/section[1]/div/ul/li/div/div[2]/div[3]/span[2]").text)
         pe=driver.find_element_by_xpath("/html/body/main/section/section/div[2]/div/div/section[1]/div/ul/li/div/div[2]/div[3]/span[2]").text
+        enviarWhatsapp("POR: "+pe)
         tablaLog.insert(parent="",index="end",text="parent",values=("Encontrado: "+pe))
         comprar()
-    # else:
-    #     print("NO SE ENCONTRO")
-        
+
+#CLICK PRIMER ELEMENTO ENCONTRADO SOLO SI LO ENCUENTRA NO LO VENDE
+def clickEncontradoSinVender():
+    if len(driver.find_elements_by_xpath("/html/body/main/section/section/div[2]/div/div/section[1]/div/ul/li/div")) > 0:
+        driver.find_element_by_xpath("/html/body/main/section/section/div[2]/div/div/section[1]/div/ul/li/div").click()
+        print(driver.find_element_by_xpath("/html/body/main/section/section/div[2]/div/div/section[1]/div/ul/li/div/div[2]/div[3]/span[2]").text)
+        pe=driver.find_element_by_xpath("/html/body/main/section/section/div[2]/div/div/section[1]/div/ul/li/div/div[2]/div[3]/span[2]").text
+        enviarWhatsapp("POR: "+pe)
+        tablaLog.insert(parent="",index="end",text="parent",values=("Encontrado: "+pe))
+        comprarSinVender()        
 
 #CLICK REGRESAR A BUSQUEDA DE MERCADO DE TRANSFERENCIA
 def clickRegresar():
@@ -107,7 +120,11 @@ def comprar():
         driver.find_element_by_xpath("/html/body/div[4]/section/div/div/button[1]").click()
         time.sleep(5)
         if float(driver.find_element_by_xpath("/html/body/main/section/section/div[1]/div[1]/div[1]").text) != float(saldoInicial):
+            saldo=driver.find_element_by_xpath("/html/body/main/section/section/div[1]/div[1]/div[1]").text
+            print(saldo)
             print("SE COMPRO")
+            enviarWhatsapp("SE COMPRO")
+            enviarWhatsapp(saldo)
             # saldo=driver.find_element_by_xpath("/html/body/main/section/section/div[1]/div[1]/div[1]").text
             time.sleep(3)
             if len(driver.find_elements_by_xpath("/html/body/main/section/section/div[2]/div/div/section[2]/div/div/div[2]/div[2]/div[1]/button")) > 0:
@@ -115,19 +132,92 @@ def comprar():
                 time.sleep(2)
                 definirPrecio()
             else:
-                print("ALGO SALIO MAL")
+                enviarWhatsapp("FALSA ALARMA")
+                print("FALSA ALARMA")
         else:
             print("NO SE COMPRO")
+            enviarWhatsapp("NO SE COMPRO")
         time.sleep(5)
     else:
+        enviarWhatsapp("FALSA ALARMA")
+        print("FALSA ALARMA")
+
+## COMPRA EL ARTICULO Y LO PONER EN EL MERCADO
+def comprarSinVender():
+    saldoInicial=driver.find_element_by_xpath("/html/body/main/section/section/div[1]/div[1]/div[1]").text
+    driver.find_element_by_xpath("/html/body/main/section/section/div[2]/div/div/section[2]/div/div/div[2]/div[2]/button[2]").click()
+    if len(driver.find_elements_by_xpath("/html/body/div[4]/section/div/div/button[1]")) > 0:
+        driver.find_element_by_xpath("/html/body/div[4]/section/div/div/button[1]").click()
+        time.sleep(2)
+        if float(driver.find_element_by_xpath("/html/body/main/section/section/div[1]/div[1]/div[1]").text) != float(saldoInicial):
+            print("SE COMPRO")
+            enviarWhatsapp("SE COMPRO")
+            # saldo=driver.find_element_by_xpath("/html/body/main/section/section/div[1]/div[1]/div[1]").text
+            # time.sleep(3)
+            if len(driver.find_elements_by_xpath("/html/body/main/section/section/div[2]/div/div/section[2]/div/div/div[2]/div[2]/div[1]/button")) > 0:
+                print("SI")
+            else:
+                enviarWhatsapp("FALSA ALARMA")
+                print("FALSA ALARMA")
+        else:
+            print("NO SE COMPRO")
+            enviarWhatsapp("NO SE COMPRO")
+        # time.sleep(5)
+    else:
+        enviarWhatsapp("FALSA ALARMA")
         print("FALSA ALARMA")
 
 
 #######################################################################
 ####################################################################### 
-#######################################################################
+#######################  WHATSAPP #####################################
 #######################################################################
 #######################################################################  
+
+## ENVIAR WHATSAPP
+def enviarWhatsapp(mensaje):
+    posicionar=driverWhatsapp.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]")
+    posicionar.click()
+    time.sleep(2)
+    posicionar.send_keys(mensaje)
+    botonEnviar=driverWhatsapp.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[3]/button/span")
+    botonEnviar.click()
+    print("WHATSAPP ENVIADO")
+
+def enviarImagenWhatsapp():
+    posicionar=driverWhatsapp.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]")
+    posicionar.click()
+    posicionar.send_keys(Keys.CONTROL, 'v') #paste
+    time.sleep(3)
+    botonEnviar=driverWhatsapp.find_element_by_xpath("//*[@id='app']/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/span/div/div")
+    botonEnviar.click()
+    print("IMAGEN ENVIADA")
+
+##TOMA UN SCREENSHOT DE LA PANTALLA Y GUARDARLA EN EL CLIPBOARD
+def enviarScreenshot():
+    driver.save_screenshot("screenshot.png");
+    filepath = 'screenshot.png'
+    image = Image.open(filepath)
+    output = BytesIO()
+    image.convert("RGB").save(output, "BMP")
+    data = output.getvalue()[14:]
+    output.close()
+    send_to_clipboard(win32clipboard.CF_DIB, data)
+    enviarImagenWhatsapp()
+
+def send_to_clipboard(clip_type, data):
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+    win32clipboard.SetClipboardData(clip_type, data)
+    win32clipboard.CloseClipboard()
+
+#######################################################################
+####################################################################### 
+#######################################################################
+#######################################################################
+####################################################################### 
+
+
 
 
 
@@ -146,6 +236,7 @@ def stop():
     seguir = False
 
 
+
 ## INICIAR A BUSCAR Y COMPRAR
 def iniciar():
     print("INICIO")
@@ -161,7 +252,7 @@ def iniciar():
     inicial.insert(0,precioFinal-1000)
     print("GANANCIA MINIMA")
     print(float(precioFinal)*.95-int(maximo.get()))
-    print(driver.find_element_by_xpath("/html/body/main/section/section/div[2]/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/input").text)
+    # print(driver.find_element_by_xpath("/html/body/main/section/section/div[2]/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/input").text)
     tablaLog.insert(parent="",index="end",text="parent",values=("Jugador:"+driver.find_element_by_xpath("/html/body/main/section/section/div[2]/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/input").text.replace(" ", "")))
     
     gananciaMinima = "GananciaMinima:"+str(float(precioFinal)*.95-int(maximo.get()))
@@ -184,7 +275,9 @@ def iniciar():
                 # print(iteraciones)
                 if iteraciones==30:
                     print("ESPERANDO 45 SEGUNDOS")
+                    enviarWhatsapp("ESPERANDO 45 SEGUNDOS")
                     time.sleep(45)
+                    enviarWhatsapp("REINICIADO")
                     print("REINICIANDO")
                     iteraciones=0
                 if espera==3:
@@ -208,9 +301,84 @@ def iniciar():
                 if switch == False:  
                     break
         except Exception:
-            print("ALGO OCURRIO")  
+            print("ALGO OCURRIO") 
+            enviarWhatsapp("ALGO OCURRIO")
+            enviarScreenshot() 
     thread = threading.Thread(target=run)
     thread.start()  
+
+
+## INICIAR A BUSCAR Y COMPRAR
+def iniciarSinVender():
+    print("INICIO")
+    tablaLog.insert(parent="",index="end",text="parent",values=("Iniciado" ))
+    saldo=driver.find_element_by_xpath("/html/body/main/section/section/div[1]/div[1]/div[1]").text
+    print(saldo)
+    tablaLog.insert(parent="",index="end",text="parent",values=("SaldoActual:"+saldo))
+    #TOMA PRECIO FINAL DE CAMPO 
+    # precioFinal=int(final.get()) 
+    # maximo.delete(0,END)
+    # maximo.insert(0,int(precioFinal/1.07))
+    # inicial.delete(0,END)
+    # inicial.insert(0,precioFinal-1000)
+    # print("GANANCIA MINIMA")
+    # print(float(precioFinal)*.95-int(maximo.get()))
+    # # print(driver.find_element_by_xpath("/html/body/main/section/section/div[2]/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/input").text)
+    # tablaLog.insert(parent="",index="end",text="parent",values=("Jugador:"+driver.find_element_by_xpath("/html/body/main/section/section/div[2]/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/input").text.replace(" ", "")))
+    
+    # gananciaMinima = "GananciaMinima:"+str(float(precioFinal)*.95-int(maximo.get()))
+    # # print(gananciaMinima)
+    # tablaLog.insert(parent="",index="end",text="parent",values=(gananciaMinima))
+
+    # # tablaLog.insert(parent="",index="end",text="parent",values=(gananciaMinima ))
+    global switch  
+    switch = True
+
+    #DEFINE EL PRECIO MAXIMO DE BUSQUEDA
+    precioMaximoBusqueda()
+    def run():
+        try:
+            global espera
+            espera=3
+            while seguir:
+                global iteraciones
+                iteraciones=iteraciones+1
+                # print(iteraciones)
+                if iteraciones==30:
+                    print("ESPERANDO 45 SEGUNDOS")
+                    enviarWhatsapp("ESPERANDO 45 SEGUNDOS")
+                    time.sleep(45)
+                    enviarWhatsapp("REINICIADO")
+                    print("REINICIANDO")
+                    iteraciones=0
+                if espera==3:
+                    aumentaMinimoCompraYa()
+                    espera=espera+1
+                else: 
+                    disminuyeMinimoCompraYa()
+                    espera=espera-1
+                time.sleep(int(espera))
+                if switch == False:  
+                    break  
+                clickBuscar()
+                time.sleep(1)
+                if switch == False:  
+                    break  
+                clickEncontradoSinVender()
+                time.sleep(2)
+                if switch == False:  
+                    break  
+                clickRegresar()
+                if switch == False:  
+                    break
+        except Exception:
+            print("ALGO OCURRIO") 
+            enviarWhatsapp("ALGO OCURRIO")
+            enviarScreenshot() 
+    thread = threading.Thread(target=run)
+    thread.start()  
+
+
 
 def switchon():    
     global switch  
@@ -249,9 +417,12 @@ frameJugadores = LabelFrame(root)
 ## BOTON INICIAR
 iniciar = Button(frameControles, text="Iniciar", command=iniciar, width=7, height=3)
 iniciar.grid(row=0,column=0)
+## BOTON INICIAR
+iniciarSinVender = Button(frameControles, text="Iniciar Sin Comprar", command=iniciarSinVender, width=7, height=3)
+iniciarSinVender.grid(row=1,column=1)
 ## BOTON FINALIZAR
 finalizar = Button(frameControles, text="Detener", command=switchoff, width=7, height=3)
-finalizar.grid(row=0,column=1)
+finalizar.grid(row=0,column=2)
 
 
 ###################### COMPRA
